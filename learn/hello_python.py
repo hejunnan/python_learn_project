c808c8a33ac83c5238cb5f4afa6aa1def6079be9
hejunnan <hejunnan94@qq.com>
2022-02-09 19:33:21 +0800

    【项目新增】 hello world和字符串学习 【时间花费】 1min

---
+++ learn/hello_python.py
@@ -0,0 +1,5 @@
+# 第一行python代码
+print("hello world")
+
+
+
---
+++ learn/string.py
@@ -0,0 +1,17 @@
+# 变量命名
+message = "hello python"
+print(message)
+
+# 字符串使用方法
+# 单词首字母大写
+print(message.title())
+# 字符串全转换为大写
+print("python".upper())
+# 字符串全转换为小写
+print(message.lower())
+
+# f字符串 f是format的缩写
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
